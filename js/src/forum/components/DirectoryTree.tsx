1bcf3d02fac6f8de5d26bb44ef152e6bd53e119a
William <nearata@protonmail.com>
2023-02-09 17:45:56 +0100

    add placeholder if folder is empty

--- js/src/forum/components/DirectoryTree.tsx
+++ js/src/forum/components/DirectoryTree.tsx
@@ -3,6 +3,8 @@ import DirectoryTreeBackItem from "./DirectoryTreeBackItem";
 import DirectoryTreeItem from "./DirectoryTreeItem";
 import Component from "flarum/common/Component";
 import LoadingIndicator from "flarum/common/components/LoadingIndicator";
+import Placeholder from "flarum/common/components/Placeholder";
+import app from "flarum/forum/app";
 import type Mithril from "mithril";
 
 type Attrs = {
@@ -28,9 +30,16 @@ export default class DirectoryTree extends Component<Attrs> {
       return <LoadingIndicator />;
     }
 
+    const placeholderText = app.translator.trans(
+      "nearata-directory-listing.forum.placeholder_empty_folder_text"
+    );
+
     return (
       <div class="DirectoryTreeList">
         <DirectoryTreeBackItem />
+        {!this.directoryState.getData().length && (
+          <Placeholder text={placeholderText} />
+        )}
         {this.directoryState.getData().map(function (val) {
           return <DirectoryTreeItem data={val} />;
         })}
